6340eda05887e126b1990c6db01e50f1624bf626
muchsinhisyam <muchsin.ht@gmail.com>
2020-06-11 01:11:00 +0800

    Creating similarity based Music Recommendation

--- SimilarityRecommendation.py
+++ SimilarityRecommendation.py
@@ -182,9 +182,42 @@ print(train.head(5))
 #Create an instance of the class
 pm = Recommenders.popularity_recommender()
 pm.create(train, 'user_id', 'song')
-user_id1 = u[5]
-#Recommended songs list for a user
-pm.recommend(user_id1)
 
-user_id2 = u[8]
-pm.recommend(user_id2)
+# user_id1 = u[5]
+# #Recommended songs list for a user
+# pm.recommend(user_id1)
+# user_id2 = u[8]
+# pm.recommend(user_id2)
+
+is_model = Recommenders.similarity_recommender()
+is_model.create(train, 'user_id', 'song')
+
+#Print the songs for the user
+user_id1 = u[5]
+user_items1 = is_model.get_user_items(user_id1)
+print("------------------------------------------------------------------------------------")
+print("Songs played by first user %s:" % user_id1)
+print("------------------------------------------------------------------------------------")
+for user_item in user_items1:
+    print(user_item)
+print("----------------------------------------------------------------------")
+print("Similar songs recommended for the first user:")
+print("----------------------------------------------------------------------")
+#Recommend songs for the user using personalized model
+is_model.recommend(user_id1)
+
+user_id2 = u[7]
+#Fill in the code here
+user_items2 = is_model.get_user_items(user_id2)
+print("------------------------------------------------------------------------------------")
+print("Songs played by second user %s:" % user_id2)
+print("------------------------------------------------------------------------------------")
+for user_item in user_items2:
+    print(user_item)
+print("----------------------------------------------------------------------")
+print("Similar songs recommended for the second user:")
+print("----------------------------------------------------------------------")
+#Recommend songs for the user using personalized model
+is_model.recommend(user_id2)
+
+is_model.similar_items(['U Smile - Justin Bieber'])
