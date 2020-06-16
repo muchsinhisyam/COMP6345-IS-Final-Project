7a3738f91e19b22766c98e0f46c0396f52f1f8c3
Alifio Rasyid <official.alifio@gmail.com>
2020-06-17 01:37:57 +0700

    Update main.py, insyallah last push

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ redirect_uri = 'http://google.com/'
 username = ''
 scope = 'user-top-read'
 
+token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+spotifyObject = spotipy.Spotify(auth=token)
+
 # Read user_id, song_id, listen_count
 # This step might take time to download data from external sources
 triplets = 'data/10000.txt'
@@ -25,9 +28,8 @@ songs_metadata = 'data/song_data.csv'
 
 song_df_a = pandas.read_table(triplets, header=None)
 song_df_a.columns = ['user_id', 'song_id', 'listen_count']
-
-# Read song  metadata
 song_df_b = pandas.read_csv(songs_metadata)
+
 song_df1 = pandas.merge(song_df_a, song_df_b.drop_duplicates(['song_id']), on="song_id", how="left")
 song_df1.head()
 song_df1 = song_df1.head(20000)
@@ -90,9 +92,45 @@ is_model.create_s(train, 'user_id', 'song')
 # is_model.recommend_s(user_id2)
 #
 
+# Get Spotify Device ID
+def get_device_id(device_type):
+    device_id = ""
+    # Get list of devices
+    devices = spotifyObject._get('me/player/devices')['devices']
+    if len(devices) == 0:
+        return 1
+    for device in devices:
+        if device['type'] == device_type:
+            return device['id']
+    return 2
+
+
+def play_spotify(results):
+    current_market = spotifyObject.current_user()['country']
+    searchResult = spotifyObject.search(results[0][1], type='track', limit=1, market=current_market)
+    try:
+        track_uri = searchResult['tracks']['items'][0]['uri']
+    except IndexError:
+        print()
+        print("Song is not available on spotify")
+        return
+
+    device_id = get_device_id("Computer")
+    if (device_id == 1) or (device_id == 2):
+        print()
+        print("Can't play song since Spotify is inactive.")
+        return
+
+    data = {'uris': [track_uri]}
+    url = 'me/player/play?device_id=' + device_id
+    spotifyObject._put(url, payload=data)
+
+    print()
+    print("Playing " + results[0][1] + ' on spotify')
+
 
 while True:
-    print('\n')
+    print('')
     print('Welcome to our Music Recommendation System')
     print('1 - Get Recommendation based on song searched')
     print('2 - Get Recommendation based on your spotify top tracks')
@@ -107,21 +145,23 @@ while True:
         for result in results:
             print(it, result[1])
             it += 1
+        play_spotify(results)
 
     elif options == 2:
-        token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-        spotifyObject = spotipy.Spotify(auth=token)
         toptracks = spotifyObject._get('me/top/tracks?time_range=long_term')['items']
         data = []
+
         for tracks in toptracks:
             temp = tracks['name'] + ' - ' + tracks['artists'][0]['name']
             data.append(temp)
+
         results = is_model.similar_items(data).to_numpy()
         it = 1
-        print('\nSongs:')
+        print('\nRecommended Songs:')
         for result in results:
             print(it, result[1])
             it += 1
+        play_spotify(results)
 
     else:
         sys.exit(0)
